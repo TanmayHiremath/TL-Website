4c1787916122ea46ef2cde2fc3003c3fbb2e3ff7
TanmayHiremath <tanmay.hiremath@gmail.com>
2020-05-06 17:21:11 +0530

    overlay
    overlay for automated machines

--- TLfront/src/app/home/home.component.ts
+++ TLfront/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    
+    $(document).ready(function(){
+      $(".card").mouseover(function(){
+        $(".card").css("cursor","pointer")
+      });
+      $(".card-body").click(function(){
+        $(this).siblings(".overlay").show()
+        
+      });
+      $(".closeOverlay").click(function(){
+        $(".overlay").hide()
+      });
+     
+    });
   }
 
 }
