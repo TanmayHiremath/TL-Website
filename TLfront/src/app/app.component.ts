b52075559e223a0c0bb61d3f7c183707ddb58351
TanmayHiremath <tanmay.hiremath@gmail.com>
2020-05-12 23:11:23 +0530

    go to top,navbar color change

--- TLfront/src/app/app.component.ts
+++ TLfront/src/app/app.component.ts
@@ -10,17 +10,48 @@ import * as $ from 'jquery';
 export class AppComponent implements OnInit {
   title = 'TLfront';
   year: string;
+  flag;
 
   constructor() {
 
-
+    this.flag=0;
     this.year = new Date().getFullYear().toString();
 
   }
 
   ngOnInit(): void {
+    
+    
+    $(document).ready(function () {
+
+     $(document).scroll(function (){
+       if($(window).scrollTop()>700)
+       {
+        document.getElementById("scrollBtn").style.display="block";
+        $(".nav").css({"background":"black","position":"sticky","top":"0"})
+       }
+       else
+       {document.getElementById("scrollBtn").style.display="none";
+         $(".nav").css({"background":"#20c9c3","position":"static"})}
+       
+
+     });
+
+     $("#scrollBtn").click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+    }, 1200);
+     });
+       
+     
+    });
 
   }
+  
+
 
 
 }
+
+
+
