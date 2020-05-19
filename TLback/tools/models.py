904cde2165ad4f6a68fea3b1a9df9ffa26716a45
TanmayHiremath <tanmay.hiremath@gmail.com>
2020-05-19 20:53:57 +0530

    migrated

--- TLback/tools/models.py
+++ TLback/tools/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
 # Create your models here.
 class Item(models.Model):
+    
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=30)
     description = models.CharField(max_length=2000)
