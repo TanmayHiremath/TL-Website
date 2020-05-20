21b12db618b0a813768fb29e8c5af5be2e5ba325
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-21 04:03:07 +0530

    Update admin.py

--- TLback/tools/admin.py
+++ TLback/tools/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Item, Request
+from .models import *
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Request)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(Customer)
