3db013f4722ce3f9a2dec5653dcd4f49d79c592c
aditakarsh27 <aditakarsh27@gmail.com>
2020-05-22 02:07:19 +0530

    Added some components
    How do I send parameters?

--- TLback/tools/serializers.py
+++ TLback/tools/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import *
+from .models import Item, Request, Order, OrderProduct, Customer
 
 
 class ItemSerializer(serializers.ModelSerializer):
