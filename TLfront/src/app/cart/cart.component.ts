a1fca5fcddab733c426745ad73f303edd79ecf93
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-21 15:27:40 +0530

    Update cart.component.ts

--- TLfront/src/app/cart/cart.component.ts
+++ TLfront/src/app/cart/cart.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  providers: [ApiService]
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  orders = [{customer:'titanic'}]
+
+  constructor(private api: ApiService) { 
+    
+  }
 
   ngOnInit(): void {
+   
+      this.api.getOrders().subscribe(
+        data => {
+          this.orders = data;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    
   }
 
 }
