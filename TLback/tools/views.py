33f0a95fb5ad8fd273feae2ac10cf764cf141c8c
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-21 15:27:32 +0530

    Update views.py

--- TLback/tools/views.py
+++ TLback/tools/views.py
@@ -3,14 +3,29 @@ from .models import *
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from tools.serializers import ItemSerializer
-from .models import Item
+from tools.serializers import *
+from .models import *
 from rest_framework.response import Response
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderProductViewSet(viewsets.ModelViewSet):
+    queryset = OrderProduct.objects.all()
+    serializer_class = OrderProductSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
 def store(request):
 	items=Item.objects.all()
 	context = {'items':items}
