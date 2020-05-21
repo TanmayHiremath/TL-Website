9e7ab41d3fae83581817ae321952c795438ee1dd
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-21 06:39:24 +0530

    Update views.py

--- TLback/tools/views.py
+++ TLback/tools/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from .models import *
 
 def store(request):
-	context = {}
+	items=Item.objects.all()
+	context = {'items':items}
 	return render(request, 'tools/store.html', context)
 
 def cart(request):
-	context = {}
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		products = order.orderproduct_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		products = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+
+	context = {'products':products, 'order':order}
 	return render(request, 'tools/cart.html', context)
 
 def checkout(request):
