9a260259de86268a8f18ecd3de5abab6d5aa98ff
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-21 06:55:03 +0530

    Update views.py

--- TLback/tools/views.py
+++ TLback/tools/views.py
@@ -15,11 +15,20 @@ def cart(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		products = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = { 'get_cart_products':0}
 
 	context = {'products':products, 'order':order}
 	return render(request, 'tools/cart.html', context)
 
 def checkout(request):
-	context = {}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		products = order.orderproduct_set.all()
+	else:
+		#Create empty cart for now for non-logged in user
+		products = []
+		order = { 'get_cart_products':0}
+
+	context = {'products':products, 'order':order}
 	return render(request, 'tools/checkout.html', context)
