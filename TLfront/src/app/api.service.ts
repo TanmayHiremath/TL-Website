b7731faecd4386b18442cbaf4efdd0f6a7936c02
adityajain1152 <adityajain1152@gmail.com>
2020-05-23 22:17:16 +0530

    Update api.service.ts

--- TLfront/src/app/api.service.ts
+++ TLfront/src/app/api.service.ts
@@ -1,44 +1,84 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { item_interface } from './item_interface';
+import { request_interface } from './request_interface';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  
+
   baseurl = "http://127.0.0.1:8000";
-  httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+  httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private http: HttpClient) { }
 
   getItems(): Observable<any> {
     return this.http.get(this.baseurl + '/items/',
-    {headers: this.httpHeaders});
+      { headers: this.httpHeaders });
   }
+
+  getRequests(): Observable<any> {
+    return this.http.get(this.baseurl + '/requests/',
+      { headers: this.httpHeaders });
+  }
+  createRequest(x): Observable<any> {
+    const body = { item: x.item, quantity: x.quantity, roll: x.roll };
+    return this.http.post<request_interface>(this.baseurl + '/requests/', body,
+      { headers: this.httpHeaders });
+  }
+
+  updateItem(item): Observable<item_interface> {
+    // const body = { title: item.title, desc: item.desc, year: item.year };
+    return this.http.put<item_interface>(this.baseurl + '/items/' + item.id + '/',
+      { headers: this.httpHeaders });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   getOrders(): Observable<any> {
-    return this.http.get(this.baseurl + '/order/',
-    {headers: this.httpHeaders});
+    return this.http.get(this.baseurl + '/orders/',
+      { headers: this.httpHeaders });
   }
 
   getOrderproducts(): Observable<any> {
     return this.http.get(this.baseurl + '/orderproduct/',
-    {headers: this.httpHeaders});
+      { headers: this.httpHeaders });
   }
 
-  getCustomer(): Observable<any> {
-    return this.http.get(this.baseurl + '/customer/',
-    {headers: this.httpHeaders});
+  getCustomers(): Observable<any> {
+    return this.http.get(this.baseurl + '/customers/',
+      { headers: this.httpHeaders });
   }
 
-  getRequest(): Observable<any> {
-    return this.http.get(this.baseurl + '/request/',
-    {headers: this.httpHeaders});
-  }
 
-  updateRequest(movie): Observable<any> {
-    const body = {title: movie.title , desc: movie.desc, year: movie.year };
-    return this.http.put(this.baseurl + '/movies/' + movie.id + '/', body,
-    {headers: this.httpHeaders});
+
+  updateRequest(req): Observable<any> {
+    const body = { item: req.item, quantity: req.quantity, roll: req.roll ,is_sent:req.is_sent };
+    return this.http.put<request_interface>(this.baseurl + '/requests/'+ req.id +'/' , body,
+      { headers: this.httpHeaders });
   }
 
 }
