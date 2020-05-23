3bcca9e7673bc4bb40ff6fda0b2515d6fe213484
adityajain1152 <adityajain1152@gmail.com>
2020-05-23 22:16:44 +0530

    Create request_interface.ts

---
+++ TLfront/src/app/request_interface.ts
@@ -0,0 +1,12 @@
+export interface request_interface {
+    
+    id:number
+    item : number
+    roll : string
+    quantity : number
+    is_sent : boolean
+    is_approved : boolean
+    is_issued : boolean
+    is_returned : boolean
+    
+}
