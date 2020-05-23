e97f8f0d28f8b50e2dde53e3bb49b95401429806
adityajain1152 <adityajain1152@gmail.com>
2020-05-23 22:17:14 +0530

    Create item_interface.ts

---
+++ TLfront/src/app/item_interface.ts
@@ -0,0 +1,27 @@
+
+
+export interface item_interface {
+    id:number
+    name : string
+    category : string
+    level1 : string
+    level2 : string
+    description : string
+    keywords : string
+    picture : File
+    id_required :boolean
+    quantity : number
+    critical_val :number
+    current_holders : string
+    is_consumable : boolean
+    colour_code : string
+    notifications : string
+    price : number
+    flag : boolean
+    flag_time : any
+
+    display:boolean
+    displaylevel1:boolean
+    displaylevel2:boolean
+
+}
