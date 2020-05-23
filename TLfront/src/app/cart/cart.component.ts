7e7c752015559bd16557b77344bd77b550821e7d
adityajain1152 <adityajain1152@gmail.com>
2020-05-23 22:16:57 +0530

    Update cart.component.ts

--- TLfront/src/app/cart/cart.component.ts
+++ TLfront/src/app/cart/cart.component.ts
@@ -15,112 +15,114 @@ export class CartComponent implements OnInit {
   products = []
   customers = []
   items = []
-  requests = []
-  total_items=0
-  
+  requests
+  total_items = 0
 
-  constructor(private api: ApiService) { 
-    
+
+  constructor(private api: ApiService) {
+    this.requests =[{ id: -1, item: -1, roll: 0,quantity:1 }]
+
   }
 
   ngOnInit(): void {
-   
-      this.api.getOrders().subscribe(
-        data => {
-          this.orders = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
 
-      this.api.getOrderproducts().subscribe(
-        data => {
-          this.products = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-      this.api.getCustomer().subscribe(
-        data => {
-          this.customers = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-      this.api.getItems().subscribe(
-        data => {
-          this.items = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-      this.api.getRequest().subscribe(
-        data => {
-          this.requests = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
-        
+    this.api.getOrders().subscribe(
+      data => {
+        this.orders = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.api.getOrderproducts().subscribe(
+      data => {
+        this.products = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    this.api.getCustomers().subscribe(
+      data => {
+        this.customers = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    this.api.getItems().subscribe(
+      data => {
+        this.items = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    this.api.getRequests().subscribe(
+      data => {
+        this.requests = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
   }
 
 
-  choose(a, b){
+  choose(a, b) {
 
-    if(a == b)
+    if (a == b)
       return true
 
-      else
+    else
       return false
 
   }
 
- 
+
   total(roll) {
 
 
-//     var i;
-//   for (i = 0; i < this.requests.length; i+=1) {
-  
-//     if( this.requests[i].is_sent == false){
-//       this.total_items+= this.requests[i].quantity
-  
-//       console.log(this.total_items)
-//       console.log(this.requests[i].quantity)
-      
-//        console.log(this.requests.length)
-      
-//       console.log("this.requests[1].quantity")
-//   }
-// }
+    //     var i;
+    //   for (i = 0; i < this.requests.length; i+=1) {
+
+    //     if( this.requests[i].is_sent == false){
+    //       this.total_items+= this.requests[i].quantity
+
+    //       console.log(this.total_items)
+    //       console.log(this.requests[i].quantity)
+
+    //        console.log(this.requests.length)
+
+    //       console.log("this.requests[1].quantity")
+    //   }
+    // }
 
 
 
-    const keys= Object.keys(this.requests)
-    this.total_items=0
+    const keys = Object.keys(this.requests)
+    this.total_items = 0
 
-    for( const key of keys) {
+    for (const key of keys) {
 
-     
 
-      if( this.requests[key].is_sent == false){
-          this.total_items+= this.requests[key].quantity
-          
-      
-         
-       }
+
+      if (this.requests[key].is_sent == false) {
+        this.total_items += this.requests[key].quantity
+        //  console.log(this.requests[key].id, "hi")
+
+
+
+      }
     }
-    
 
 
-    
 
-  }  
+
+
+  }
 
 
   updateRequest() {
@@ -132,15 +134,16 @@ export class CartComponent implements OnInit {
   
     if( this.requests[i].is_sent == false)
     {
-      
-      this.api.updateRequest().subscribe(
+      this.requests[i].is_sent = true;
+      this.api.updateRequest(this.requests[i]).subscribe(
         data => {
-          this.getRequest();
+          console.log(data)
         },
         error => {
           console.log(error);
         }
       );
+
     }
   }
 
