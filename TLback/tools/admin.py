3199a54522af8ceaf155997f6c5be393fc79baaf
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-05-28 22:42:27 +0530

    admin

--- TLback/tools/admin.py
+++ TLback/tools/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from .models import Item, Request, Order, OrderProduct, Customer
 
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ('item', 'roll', 'quantity','colour_code')
+    list_display = ('item', 'roll', 'quantity', 'colour_code', 'is_sent')
+    list_editable = ('is_sent',)
+    search_fields = ['roll', 'item__name', ]
+    # search_fields = ('roll' , )
+    # ordering = ['last_name', 'email'] # first by last_name then by email
+    list_filter = ['item', 'roll', 'quantity', 'colour_code', 'is_sent']
 # Register your models here.
+
+
 admin.site.register(Item)
 admin.site.register(Request, RequestAdmin)
 admin.site.register(Order)
