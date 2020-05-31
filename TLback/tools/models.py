b5ef477de6c542f2d1844d5c394d0c56d7e6514b
aditakarsh27 <aditakarsh27@gmail.com>
2020-06-01 02:39:00 +0530

    Model.py bad

--- TLback/tools/models.py
+++ TLback/tools/models.py
@@ -67,8 +67,9 @@ class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200)
-
-	def __str__(self):
+    code =models.CharField(max_length=200)
+	
+    def __str__(self):
 		return self.name
 
 
