3456f3c109c6f9523b262e03afc378210494c09f
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-06-03 22:01:05 +0530

    Update cart.component.ts

--- TLfront/src/app/cart/cart.component.ts
+++ TLfront/src/app/cart/cart.component.ts
@@ -32,23 +32,9 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
 
 
-    this.api.getOrders().subscribe(
-      data => {
-        this.orders = data;
-      },
-      error => {
-        console.log(error);
-      }
-    );
+    
 
-    this.api.getOrderproducts().subscribe(
-      data => {
-        this.products = data;
-      },
-      error => {
-        console.log(error);
-      }
-    );
+    
     this.api.getCustomers().subscribe(
       data => {
         this.customers = data;
