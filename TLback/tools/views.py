decc283ea7bb9e05afd5d638a348ae4f393b1a3c
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2020-06-06 14:40:03 +0530

    .

--- TLback/tools/views.py
+++ TLback/tools/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Item, Request, Customer
 
-
+from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tools.serializers import *
@@ -20,3 +20,12 @@ class CustomerViewSet(viewsets.ModelViewSet):
 class RequestViewSet(viewsets.ModelViewSet):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
+
+def post(self, request, format=None):
+        headers = {'Authorization':'Basic {Yaha base64 convert karke daalna hai}',
+                        'Content-Type':'application/x-www-form-urlencoded'
+        }
+        r = request.post('https://gymkhana.iitb.ac.in/sso/oauth/token/', data='code='+request.data['code']+'&redirect_uri={redirect_uri}&grant_type=authorization_code', headers=headers) 
+        r = request.get('https://gymkhana.iitb.ac.in/sso/user/api/user/?fields=first_name,last_name,type,profile_picture,sex,username,email,program,contacts,insti_address,secondary_emails,mobile,roll_number', headers={'Authorization':'Bearer '+r.json()['access_token']})
+        data=r.json()
+        return HttpResponse("<h1>"+data+"</h1>")
