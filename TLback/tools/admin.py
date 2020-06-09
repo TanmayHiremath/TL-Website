16b4e7f16001a969cb2e897a8641fab4d662e3d0
akshat131 <63599200+akshat131@users.noreply.github.com>
2020-06-09 17:06:23 +0530

    tech

--- TLback/tools/admin.py
+++ TLback/tools/admin.py
@@ -18,25 +18,48 @@ class UserAdmin(BaseUserAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-def sent_status(self, request, queryset):
+def issued_status(self, request, queryset):
     count= queryset.update(
-        is_sent=True
+        is_issued=True,
+        is_denied=False,
+        item__quantity= request.quantity
     )
-    self.message_user(request, '{} sent'.format(count))
-sent_status.short_description = 'sent' # at admin page
+
+    self.message_user(request, '{} issued'.format(count))
+issued_status.short_description = 'issue' # at admin page
+
+def denied_status(self, request, queryset):
+    count= queryset.update(
+        is_issued=False,
+        is_denied=True
+    )
+    
+    self.message_user(request, '{} denied'.format(count))
+denied_status.short_description = 'deny' # at admin page
+
+def returned_status(self, request, queryset):
+    count= queryset.update(
+        is_issued=True,
+        is_returned=True, is_denied=False
+    )
+    
+    self.message_user(request, '{} returned'.format(count))
+returned_status.short_description = 'return' # at admin page
+
 
 class RequestAdmin(admin.ModelAdmin):
-    list_display = ('item', 'roll', 'quantity', 'item_details', 'is_sent')
-    list_editable = ('is_sent',)
+    list_display = ('item', 'roll', 'quantity', 'item_details', 'is_issued', 'is_denied', 'is_returned')
+    list_editable = ('is_issued', 'is_denied', 'is_returned')
     search_fields = ['roll', 'item__name', 'item__colour_code' ]
-    actions = [sent_status]
+    actions = [issued_status, denied_status, returned_status]
+
     # list_select_related = (
     #     'item',
     # )
     # search_fields = ('roll' , )
     # ordering = ['last_name', 'email'] # first by last_name then by email
     #   date_hierarchy = 'publication_date'
-    list_filter = ['item', 'roll', 'quantity', 'item__colour_code', 'is_sent']
+    list_filter = ['item', 'roll', 'quantity', 'item__colour_code', 'is_issued', 'is_denied']
 # Register your models here.
 
 
