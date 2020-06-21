498de783f5125ec994ba9e674a6379feea38ec47
Tanmay Hiremath <tanmay.hiremath@gmail.com>
2020-06-21 17:01:05 +0530

    replaced profile tab with logout button for phones

--- TLfront/src/app/app.component.ts
+++ TLfront/src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent implements OnInit {
   title = 'TLfront';
   year: string;
   flag;
-  logged_in:boolean=false
+  logged_in: boolean = false
   code
-  user_data=null
-  constructor(private api: ApiService,private router: Router) {
+  user_data = null
+  constructor(private api: ApiService, private router: Router) {
 
     this.flag = 0;
     this.year = new Date().getFullYear().toString();
@@ -29,32 +29,32 @@ export class AppComponent implements OnInit {
     var urlParams = new URLSearchParams(window.location.search)
     this.code = urlParams.get('code')
     if (this.code != null) {
-      
+
       console.log(this.code)
       this.router.navigate([''])
       const that = this
-      $.post(environment.serverUrl+'autho/', { code: this.code }, function (data) {
+      $.post(environment.serverUrl + 'autho/', { code: this.code }, function (data) {
 
-        that.api.updateCustomer(data).subscribe(data=>{console.log(data),error=>{console.log(error)}})
+        that.api.updateCustomer(data).subscribe(data => { console.log(data), error => { console.log(error) } })
 
         delete data['refresh_token']
         delete data['access_token']
         console.log(data)
-        
+
         that.api.set_user_data(data)
-        data.roll_number=window.btoa(data.roll_number)
+        data.roll_number = window.btoa(data.roll_number)
         that.api.setJdata(environment.jdataKey, JSON.stringify(data))
-        
+
         window.location.reload()
-        
-        
+
+
       })
     }
     this.logged_in = this.api.is_Authenticated()
     console.log(this.logged_in)
     if (this.logged_in == true) {
-      
-      
+
+
       this.user_data = JSON.parse(this.api.getJdata(environment.jdataKey));
       this.user_data.roll_number = window.atob(this.user_data.roll_number)
 
@@ -62,14 +62,23 @@ export class AppComponent implements OnInit {
         .subscribe(data => { this.user_data = data; console.log(data), error => { console.log(error) } })
 
     }
-    else {this.router.navigate(['']);  }
+    else { this.router.navigate(['']); }
 
-    
+
     $(document).ready(function () {
-      
-      $('.dropdown-toggl').click(function(){
+
+      $('.dropdown-toggl').click(function () {
         $('.dropdown-men').fadeToggle(200)
-        
+
+      });
+
+      $('body').click(function (evt) {
+        if(evt.target.id == "dropdown-toggl" ) 
+                return; 
+            if($(evt.target).closest('#dropdown-toggl').length) 
+                return; 
+              
+            $('.dropdown-men').fadeOut(200)
       });
       $(document).scroll(function () {
         if ($(window).scrollTop() > 700) {
@@ -91,7 +100,7 @@ export class AppComponent implements OnInit {
       });
 
 
-      
+
 
       $("#uncheck").click(function () {
         $("#nav-check").prop("checked", false);
@@ -101,7 +110,7 @@ export class AppComponent implements OnInit {
 
   }
 
-  logout(){
+  logout() {
     this.api.logout()
   }
 
